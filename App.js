9b5046c6e577d10d41204f18af713e1f6ca96a49
RobertoSoares <142548339+RobertoSoares10@users.noreply.github.com>
2023-08-29 00:43:18 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import Novo from './components/Novo';
+import Hello from './components/Hello';
+import Images from './components/Images'
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+        <header className="App-header">
+        <Novo/>
+        <Hello/>
+
+          
+        
+      </header>
+     
+    </div>
+  );
+}
+
+export default App;
